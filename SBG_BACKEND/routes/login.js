c68f284549414032037928218b90e4d1406e5589
Maulik Jadav <40969469+maulik9211@users.noreply.github.com>
2020-04-25 18:03:47 +0530

    Login api updated

--- SBG_BACKEND/routes/login.js
+++ SBG_BACKEND/routes/login.js
@@ -4,7 +4,7 @@ const router=express();
 
 
 router.post("/login",(req,res,next)=>{
-    console.log(req.body);
+    // console.log(req.body);
      const username=req.body.username
      const password=req.body.password
     db.query("select * from login where UserName=? && PassWord=?",[username,password],(err,data)=>{     
@@ -15,9 +15,20 @@ router.post("/login",(req,res,next)=>{
     }
     else
     {
-        req.session.username=username;
-        res.send("Logged in successfully");
-
+        const name = data[0].Name;
+        db.query("select RoleName from role where RoleId = ?",[data[0].RoleId],(err,data)=>{
+            if(err){
+                console.log("Error in login.js");
+            }
+            else{
+                req.session.username=username;
+                res.send({
+                    userRole: data[0].RoleName,
+                    name: name
+                });
+            }
+        });
+        
     }
         
     })
