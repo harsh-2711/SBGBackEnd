cae142d8b0eec0785aa19cb7b78b13c2d604c1ab
Aman1211 <aman.sharma122111@gmail.com>
2020-04-25 18:05:23 +0530

    Forgot password

--- SBG_BACKEND/routes/login.js
+++ SBG_BACKEND/routes/login.js
@@ -1,5 +1,14 @@
 const db=require('../db')
 const express=require('express');
+const genpassword=require("generate-password");
+const nodemailer=require('nodemailer');
+const transport=nodemailer.createTransport({
+    service:"gmail",
+    auth:{
+        user:"sharma.aman1298@gmail.com",
+        pass:"aman$1234"
+    }
+    });
 const router=express();
 
 
@@ -23,6 +32,36 @@ router.post("/login",(req,res,next)=>{
     })
 
 })
+
+
+router.post("/forgot",(req,res,next)=>{
+    const email=req.body.email;
+    console.log(email);
+     const password=genpassword.generate({
+         length:7,
+         numbers:true
+     })
+     transport.sendMail({
+        to:email,
+        from:"sharma.aman1298@gmail.com",
+        subject:"Password Recovery",
+        text:"New Password:-" + password
+         
+    },(err,data)=>{
+        if(err)
+        res.status(400);
+        else
+        {
+            db.query("update login set isReset=? where UserName=?",[1,email],(err,data)=>{
+                 if(err)
+                 res.status(400)
+                 else
+                 res.status(200);
+            })
+        }
+    });
+
+    })
 
 
 module.exports=router;
