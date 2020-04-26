526a6aa7ca97b4be214b53bb0b267df7e370c811
Aman1211 <aman.sharma122111@gmail.com>
2020-04-26 18:01:06 +0530

    Changes in data api

--- SBG_BACKEND/routes/data.js
+++ SBG_BACKEND/routes/data.js
@@ -5,16 +5,18 @@ const router=express()
 
 router.post("/data",(req,res,next)=>{
     const user=req.body.user;
-   
     db.query("select * from login where UserName=?",[user],(err,data1)=>{
         if(err)
         res.status(400)
         else
         {
+            db.query("select RoleName from role where RoleId=?",[data1[0].RoleId],(err,data2)=>{
+            
               res.send({ 
                   user:data1[0].Name,
                  status:data1[0].IsReset,
-                 role:data1[0].RoleId
+                 role:data2[0].RoleName
+              })
               })
         }
     })
