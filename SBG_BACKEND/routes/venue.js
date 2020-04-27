7b207e05ced6bfd89ff8490c0b603c2512ea6993
Aman1211 <aman.sharma122111@gmail.com>
2020-04-27 15:39:33 +0530

    Venue Api and Updated Database

--- SBG_BACKEND/routes/venue.js
+++ SBG_BACKEND/routes/venue.js
@@ -4,8 +4,8 @@ const router=express()
 
 
 router.post("/add_venue",(req,res,next)=>{
-    const name=req.body.venuename
-    const cap=req.body.capacity;
+    const name=req.body.name
+    const cap=req.body.cap;
     const hasac=req.body.hasac;
     const hasproj=req.body.hasproj;
     postdata={
@@ -35,23 +35,30 @@ router.get("/venue",(req,res,next)=>{
 
 
 router.get("/venue/:id",(req,res,next)=>{
-    db.query("select * from Venue where VenueId=?",[req.params.id],(err,data)=>{
+    db.query("select * from venue where VenueId=?",[req.params.id],(err,data)=>{
         if(err)
         res.status(400)
         else
+        {
+         
         res.send(data);
+        }
     })
 })
 
 
 router.put("/edit_venue",(req,res,next)=>{
+  console.log(req.body)
     const id=req.body.id;
-    const name=req.body.venuename;
-    const cap=req.body.capacity
-    const hasac=req.body.hasac;
-    const hasproj=req.body.hasproj;
-
-
+    const name=req.body.name;
+    const cap=req.body.cap;
+    let hasac=req.body.hasac;
+    let hasproj=req.body.hasproj;
+    if(hasac=='')
+    hasac=false
+    if(hasproj=='')
+    hasproj=false
+    
     db.query("update venue set VenueName=?,Capacity=?,HasAc=?,HasProj=? where VenueId=?",[name,cap,hasac,hasproj,id],(err,data)=>{
 
         if(err)
