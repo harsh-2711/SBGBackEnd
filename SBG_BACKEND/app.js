da862ce854b7682bef28603d93f07e82976f0756
Maulik Jadav <40969469+maulik9211@users.noreply.github.com>
2020-05-06 16:53:32 +0530

    Added APIs for suggestion box

--- SBG_BACKEND/app.js
+++ SBG_BACKEND/app.js
@@ -13,6 +13,7 @@ const subRouter=require("./routes/subscriber");
 const lostfoundRouter=require("./routes/lostfound");
 const eventlifeCycle=require("./routes/eventcycle");
 const complaintRouter=require("./routes/complaints");
+const suggestionRouter=require("./routes/suggestions");
 const session=require("express-session")
 
 const filestorage=multer.diskStorage(
@@ -44,6 +45,7 @@ app.use(eventRouter);
 app.use(eventlifeCycle);
 app.use(chatRouter);
 app.use(complaintRouter);
+app.use(suggestionRouter);
 app.use(subRouter);
 app.use(lostfoundRouter);
 const PORT = 8081;
