e1d01a6ba67dd04e434183c2cd10583e793e8878
harsh-2711 <harshaudi27@gmail.com>
2020-05-24 20:15:03 +0530

    Added mistakenly deleted db file

---
+++ SBG_BACKEND/db.js
@@ -0,0 +1,16 @@
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'sbg_daiict'
+});
+
+connection.connect(function(err) {
+    if (err) {
+        console.log(err);
+        throw err;
+    }
+});
+
+module.exports = connection;
